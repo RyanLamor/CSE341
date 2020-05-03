79173f5529a178306999c2196207fcecbb6f7f41
RyanLamor <Ryanlamor@gmail.com>
2020-05-02 23:05:02 -0600

    Week 2 prove assignment

---
+++ web/Homepage/homepage.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <!--Header-->
+	<div class="header">
+		<header>
+			<h1>Ryan's CSE 341 Homepage</h1>
+		</header>
+	</div>
+
+	<!--Menu Navigation Bar-->
+	<div class="nav_menu">
+		<a id="currentPage" href="#">Home</a>
+		<a href="assignments.html">Assignments</a>
+	</div>
+
+  <!--Main Body-->
+  <div>
+    <p>My name is Ryan Lamoreaux. I am from the wonderful sin city Las Vegas, NV, where
+       I come from a family of 6. I am the oldest sibling and cousin in my family.
+       I currently am a Junior studying Computer Science at BYUI. I have been married
+       for just over one year. I love playing video/board games, rock climbing and cycling.
+  </div>
+
+  <!-- Photo Grid -->
+  <div class="row">
+    <div class="column">
+      <img src="MeAndWife.jpg" alt="Me and Wife at SixFlags Clifornia, 2019" style="width:100%">
+    </div>
+
+    <div class="column">
+      <img src="rockClimbing.jpg" alt="Rock Climbing at Red Rock Nevada, 2015" style="width:100%">
+    </div>
+
+    <div class="column">
+      <img src="Biking.jpg" alt="Church of Jesus Christ of Latter-Day Saints, North Las Vegas Stake, 500 mile bike ride, 2015" style="width:100%;">
+    </div>
+  </div>
+
+  <br>
+  <?php
+    date_default_timezone_set('America/Boise'); // CDT
+    echo "Today is " , date('d/m/Y'), "\nThe current time is ", date('H:i:s');
+  ?>
+
+  <!--Footer-->
+  <div class="footer">
+    <footer><h2>Thanks for visiting!</h2></footer>
+  </div>
+</body>
+
+</html>
