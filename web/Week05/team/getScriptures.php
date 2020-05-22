3b80af722e661f21e73105987df8281835f05f78
RyanLamor <Ryanlamor@gmail.com>
2020-05-22 01:29:34 -0600

    fixed week05 team assign

--- web/Week05/team/getScriptures.php
+++ web/Week05/team/getScriptures.php
@@ -5,21 +5,13 @@
   $book = $_POST['book'];
   $books = "";
 
-  $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
-  $statement->execute();
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+  {
+  	$book = $row['book'];
+  	$chapter = $row['chapter'];
+  	$verse = $row['verse'];
+  	$content = $row['content'];
 
-  echo "test";
-  // Go through each result
-//  while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-  //{
-  	// The variable "row" now holds the complete record for that
-  	// row, and we can access the different values based on their
-  	// name
-  	//$book = $row['book'];
-  	//$chapter = $row['chapter'];
-  ///	$verse = $row['verse'];
-  ///	$content = $row['content'];
-
-    //$books .= "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p><br>";
-//  }
+    $books .= "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p><br>";
+  }
  ?>
