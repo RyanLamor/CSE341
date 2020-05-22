6d5d08c5f3180d77afb716ee4528cfdc142842db
RyanLamor <Ryanlamor@gmail.com>
2020-05-22 01:35:39 -0600

    fixed week05 team assign

--- web/Week05/team/getScriptures.php
+++ web/Week05/team/getScriptures.php
@@ -5,14 +5,29 @@
   $book = $_POST['book'];
   $books = "";
 
-foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
-  {
-  	$book = $row['book'];
-  	$chapter = $row['chapter'];
-  	$verse = $row['verse'];
-  	$content = $row['content'];
+  $stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptures WHERE book=:book');
+  $stmt->bindValue(':book', $book, PDO::PARAM_INT);
+  $stmt->execute();
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $books .= "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p><br>";
-  }
+  foreach ($rows as $row)
+    {
+    	$book = $row['book'];
+    	$chapter = $row['chapter'];
+    	$verse = $row['verse'];
+    	$content = $row['content'];
+
+      $books .= "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p><br>";
+    }
+
+//foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+  //{
+  	//$book = $row['book'];
+  	//$chapter = $row['chapter'];
+  	//$verse = $row['verse'];
+  	//$content = $row['content'];
+
+    //$books .= "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p><br>";
+  //}
   echo $books;
  ?>
