e0f5d8632e595439a6a879b15badda4d1aabc1ea
RyanLamor <Ryanlamor@gmail.com>
2020-05-22 15:10:26 -0600

    updated project

--- web/Week05/prove/mapHighScores.php
+++ web/Week05/prove/mapHighScores.php
@@ -3,7 +3,11 @@
   $db = get_db();
   $Map_id = $_POST['map_id'];
 
-  $stmt = $db->prepare('SELECT name, singleplayerhighscores FROM maps WHERE map_id=:map_id');
+  $stmt = $db->prepare('SELECT m.name, gh.score, gh.time, gh.datecreated FROM singleplayergamehistory gh, maps m
+  WHERE gh.isHighScore = true
+  AND gh.map_id = m.map_id
+  AND m.map_id=:map_id
+  ORDER BY gh.score');
   $stmt->bindValue(':map_id', $Map_id, PDO::PARAM_INT);
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -15,5 +19,5 @@
 
       echo "<p>" . $name . " " . $scores . "</p><br>";
     }
-  
+
  ?>
