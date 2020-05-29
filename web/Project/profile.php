f9d8391153a837b4433f0618e25f39760089fe1c
RyanLamor <Ryanlamor@gmail.com>
2020-05-29 14:21:47 -0600

    updated project

--- web/Project/profile.php
+++ web/Project/profile.php
@@ -3,12 +3,15 @@
   require "dbConnect.php";
   $db = get_db();
 
-  $stmt = $db->prepare('SELECT screen_name, friendlist FROM users WHERE username=:user_id');
-  $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+  $userID =  $_SESSION['userID'];
+
+  $stmt = $db->prepare('SELECT username, screen_name, friendlist FROM users WHERE user_id=:user_id');
+  $stmt->bindValue(':user_id', $userID, PDO::PARAM_STR);
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
   $username = $rows[0]['username'];
+  $screen_name = $rows[0]['screen_name'];
   $friends = $rows[0]['friendlist'];
 
   $stmt = $db->prepare('SELECT m.name, gh.score, gh.time, gh.datecreated FROM maps m, users u, singleplayergamehistory gh
@@ -16,7 +19,7 @@
     AND u.user_id = :user_id
     AND gh.map_id = m.map_id
     ORDER BY gh.datecreated');
-  $stmt->bindValue(':user_id', $_SESSION['userID'], PDO::PARAM_INT);
+  $stmt->bindValue(':user_id', $userID, PDO::PARAM_INT);
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -30,7 +33,7 @@
   <div>
     <header>
       <?php
-        echo "<h1>" . $username . "</h1>";
+        echo "<h1>" . $screen_name . "</h1>";
         echo "<h1>Recent Games Played</h2>";
         foreach ($rows as $row){
           echo "<p>" . $row['name'] . " " . $row['score'] . " " . $row['time'] . " " . $row['datecreated'] . "</p>";
