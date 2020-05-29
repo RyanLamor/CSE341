df97c2c5a02ca93a3a1aa90a40159f5432ce9226
RyanLamor <Ryanlamor@gmail.com>
2020-05-29 14:10:50 -0600

    updated project

--- web/Project/login.php
+++ web/Project/login.php
@@ -2,14 +2,7 @@
 <html lang="en">
 <head>
   <title>Main Screen</title>
-  <?php include 'head.php';?>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <?php include('head.php');?>
   <script src="loginCheck.js"></script>
 </head>
 <body>
--- web/Project/main.php
+++ web/Project/main.php
@@ -5,14 +5,7 @@
 <html lang="en">
 <head>
   <title>Main Screen</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
+  <?php include('head.php');?>
 </head>
 <body>
   <div>
@@ -25,7 +18,7 @@
     <form action="menuChoice.php" method="post">
       <button type="submit" value="play.html" name="btn">Play</button><br>
       <button type="submit" value="highScores.html" name="btn">High Scores</button><br>
-      <button type="submit" value="profile.php" name="btn">Profile</button><br>
+      <button type="submit" value="profile." name="btn">Profile</button><br>
       <button type="submit" value="info.html" name="btn">Game Info</button><br>
     </form>
   </div>
