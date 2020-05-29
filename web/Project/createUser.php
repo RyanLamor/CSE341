8728c73f3d253e3924aa23d33285583c409b0681
RyanLamor <Ryanlamor@gmail.com>
2020-05-29 15:06:34 -0600

    updated project

--- web/Project/createUser.php
+++ web/Project/createUser.php
@@ -7,28 +7,35 @@
   $screen_name = $_POST['screen_name'];
   $password = $_POST['password'];
 
-  $stmt = $db->prepare('INSERT INTO users (username, screen_name, password) VALUES (:username, :screen_name, :password) ');
-  $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-  $stmt->bindValue(':screen_name', $screen_name, PDO::PARAM_STR);
-  $stmt->bindValue(':password', $password, PDO::PARAM_STR);
-  $success = $stmt->execute();
+  try{
+    $stmt = $db->prepare('INSERT INTO users (username, screen_name, password) VALUES (:username, :screen_name, :password) ');
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':screen_name', $screen_name);
+    $stmt->bindValue(':password', $password);
+    $success = $stmt->execute();
 
-  $_SESSION['userID'] = $pdo->lastInsertId('user_id_seq');
-  /*
-  if ($success){
-    echo "<script>
-    alert('New User Created');
-    window.location.href='main.php';
-    </script>";
+    $_SESSION['userID'] = $pdo->lastInsertId('user_id_seq');
+    /*
+    if ($success){
+      echo "<script>
+      alert('New User Created');
+      window.location.href='main.php';
+      </script>";
+    }
+    else {
+      echo "<script>
+      console.log($success->errCode());
+      alert('Username and/or Screen name already exists');
+      window.location.href='newUser.php';
+      </script>";
+    }
+    */
   }
-  else {
-    //$success->errorcode();
-    echo "<script>
-    alert('Username and/or Screen name already exists');
-    window.location.href='newUser.php';
-    </script>";
+  catch (Exception $ex){
+    echo "Error with DB. Details: $ex";
+    die();
   }
-  */
+
 ?>
 <!DOCTYPE html>
 <html>
