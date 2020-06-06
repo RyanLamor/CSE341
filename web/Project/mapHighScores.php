a6c94249b76e99b1c37317b031f6598a4f9ecb92
RyanLamor <Ryanlamor@gmail.com>
2020-06-06 16:31:17 -0600

    updated project

--- web/Project/mapHighScores.php
+++ web/Project/mapHighScores.php
@@ -13,24 +13,20 @@
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  foreach ($rows as $row)
-    {
-      $name = $row['name'];
-      $score = $row['score'];
-      $user = $row['screen_name'];
-      $time = $row['time'];
-      $date = $row['datecreated'];
+  echo "<table class='highScoreTable' border='1'>";
+  echo "<h2>Single Player Games</h2>";
+  echo "<tr><th>Map Name</th><th>Score</th><th>Player</th><th>Time</th><th>Date</th>";
+  foreach ($rows as $row){
+    echo "<tr>";
+      foreach ($row as $field => $value) {
+          echo "<td>" . $value . "</td>";
+      }
+    echo "</tr>";
+  }
+  echo "</table>";
 
-      echo "<p>" . $name . " " . $score . " ". $user . " " . $time . " " . $date . "</p><br>";
-    }
+  echo '<a href="highScores.html" class="btn btn-light">Go Back</a>';
 
-    echo '<a href="highScores.html" class="btn btn-light">Go Back</a>';
     //update to display multiplayer games as well
-    //SELECT mulitplayerHighScores from map WHERE map_id=:map_id
-    //for each index in returned array => SELECT m.name, gh.score, u.screen_name, u2.screen_name, gh.time, gh.datecreated FROM singleplayergamehistory gh, maps m, users u, users u2
-    //WHERE gh.game_id = returned array[index]
-    //AND gh.map_id = m.map_id
-    //AND gh.player1 = u.user_id
-    //AND gh.player2 = u2.user_id
-    //AND m.map_id=:map_id
+
  ?>
