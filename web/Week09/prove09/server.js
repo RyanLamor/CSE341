670e6cabcc7cad47fbc9ccfe700343346e20012f
RyanLamor <Ryanlamor@gmail.com>
2020-06-18 19:15:33 -0600

    updated prove09

--- web/Week09/prove09/server.js
+++ web/Week09/prove09/server.js
@@ -6,7 +6,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .post('/getRate', getData)
+  .get('/getRate', getData)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function getData(req,res){
